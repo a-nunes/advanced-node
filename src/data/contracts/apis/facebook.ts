14c7b2e015c0ca5d036cdeb9bf9effd0a95993ac
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-07-18 18:36:55 -0300

    impr: ensure Service return AuthenticationError if LoadFacebookUserApi returns undefined

--- src/data/contracts/apis/facebook.ts
+++ src/data/contracts/apis/facebook.ts
@@ -1,9 +1,13 @@
 export interface LoadFacebookUserApi {
-  loadUser(params: LoadFacebookUserApi.Params): Promise<void>;
+  loadUser(
+    params: LoadFacebookUserApi.Params,
+  ): Promise<LoadFacebookUserApi.Result>;
 }
 
 export namespace LoadFacebookUserApi {
   export type Params = {
     token: string;
   };
+
+  export type Result = undefined;
 }
