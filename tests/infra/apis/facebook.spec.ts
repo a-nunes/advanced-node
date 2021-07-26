63ef50aef4dfcc80144dc4f812ce0c61c725ce96
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-07-26 20:56:39 -0300

    test: ensure FacebookApi gets debug token with correct params

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -8,26 +8,42 @@ describe('FacebookApi', () => {
   let httpClient: MockProxy<HttpGetClient>;
   let clientId: string;
   let clientSecret: string;
+  let token: string;
 
   beforeAll(() => {
-    httpClient = mock();
     clientId = 'any_client_id';
     clientSecret = 'any_client_secret';
+    token = 'any_user_token';
+    httpClient = mock();
   });
 
   beforeEach(() => {
+    httpClient.get
+      .mockResolvedValueOnce({ access_token: 'any_app_token' });
     sut = new FacebookApi(httpClient, clientId, clientSecret);
   });
 
   it('should get app token', async () => {
-    await sut.loadUser({ token: 'any_user_token' });
+    await sut.loadUser({ token });
 
     expect(httpClient.get).toHaveBeenCalledWith({
       url: 'https://graph.facebook.com/oauth/access_token',
       params: {
-        client_id: 'any_client_id',
-        client_secret: 'any_client_secret',
+        client_id: clientId,
+        client_secret: clientSecret,
         grant_type: 'client_credentials',
+      },
+    });
+  });
+
+  it('should get debug token', async () => {
+    await sut.loadUser({ token });
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: token,
       },
     });
   });
