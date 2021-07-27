3342af7d93b2cebe0af82be4efa788c432db26c9
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-07-27 19:46:11 -0300

    test: ensure AxiosHttpClient is called with correct params

---
+++ tests/infra/http/axios-client.spec.ts
@@ -0,0 +1,27 @@
+import { HttpGetClient } from '@/infra/http';
+
+import axios from 'axios';
+
+jest.mock('axios');
+
+class AxiosHttpClient {
+  async get(args: HttpGetClient.Params): Promise<void> {
+    await axios.get(args.url, { params: args.params });
+  }
+}
+
+describe('AxiosHttpClient', () => {
+  describe('get', () => {
+    it('should call get with correct params', async () => {
+      const fakeAxios = axios as jest.Mocked<typeof axios>;
+      const sut = new AxiosHttpClient();
+
+      await sut.get({ url: 'any_url', params: { data: 'any_data' } });
+
+      expect(fakeAxios.get).toHaveBeenCalledWith('any_url', {
+        params: { data: 'any_data' },
+      });
+      expect(fakeAxios.get).toHaveBeenCalledTimes(1);
+    });
+  });
+});
