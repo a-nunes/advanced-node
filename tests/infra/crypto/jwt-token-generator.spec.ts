549574dea6ccc7326fb4687f8fe14ae376e03754
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-07-28 15:56:35 -0300

    test: ensure JwtTokenGenerator rethrows if sign throws

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -9,14 +9,14 @@ describe('JwtTokenGenerator', () => {
   let fakeJwt: jest.Mocked<typeof jwt>;
   let secret: string;
   let key: string;
-  let expiration: number;
+  let expirationInMs: number;
 
   beforeAll(() => {
     fakeJwt = jwt as jest.Mocked<typeof jwt>;
     fakeJwt.sign.mockImplementation(() => 'any_generated_token');
     secret = 'private_key';
     key = 'any_key';
-    expiration = 1000;
+    expirationInMs = 1000;
   });
 
   beforeEach(() => {
@@ -24,15 +24,23 @@ describe('JwtTokenGenerator', () => {
   });
 
   it('should call sign with correct params', async () => {
-    await sut.generateToken({ key, expiration });
+    await sut.generateToken({ key, expirationInMs });
 
     expect(fakeJwt.sign).toHaveBeenCalledWith(key, secret, { expiresIn: 1 });
     expect(fakeJwt.sign).toHaveBeenCalledTimes(1);
   });
 
   it('should return token on success', async () => {
-    const result = await sut.generateToken({ key, expiration });
+    const result = await sut.generateToken({ key, expirationInMs });
 
     expect(result).toBe('any_generated_token');
+  });
+
+  it('should rethrows if sign throws', async () => {
+    fakeJwt.sign.mockImplementationOnce(() => { throw new Error('token_error'); });
+
+    const result = sut.generateToken({ key, expirationInMs });
+
+    await expect(result).rejects.toThrow(new Error('token_error'));
   });
 });
