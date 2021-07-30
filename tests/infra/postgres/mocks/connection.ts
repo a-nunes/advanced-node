d7acd176051b3499e7730255377efd13d1d39291
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-07-30 18:38:07 -0300

    chore: update connection configure to generate new uuid

--- tests/infra/postgres/mocks/connection.ts
+++ tests/infra/postgres/mocks/connection.ts
@@ -1,10 +1,12 @@
 import { v4 } from 'uuid';
-import { IMemoryDb, newDb } from 'pg-mem';
+import { DataType, IMemoryDb, newDb } from 'pg-mem';
 
 export const makeFakeDb = async (entities?: any[]): Promise<IMemoryDb> => {
   const db = newDb();
   db.public.registerFunction({
     name: 'uuid_generate_v4',
+    impure: true,
+    returns: DataType.uuid,
     implementation: () => v4(),
   });
   const connection = await db.adapters.createTypeormConnection({
