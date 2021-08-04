e9a5bdf6c17b974a2bc541ca1756dfca854aff81
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-08-04 17:57:18 -0300

    test: ensure RequiredStringValidator returns error if field is undefined

--- tests/application/validation/required-string.spec.ts
+++ tests/application/validation/required-string.spec.ts
@@ -19,4 +19,12 @@ describe('RequiredStringValidator', () => {
 
     expect(result).toEqual(new RequiredFieldError('any_field'));
   });
+
+  it('should return error if field is undefined', () => {
+    const sut = new RequiredStringValidator(undefined as any, 'any_field');
+
+    const result = sut.validate();
+
+    expect(result).toEqual(new RequiredFieldError('any_field'));
+  });
 });
