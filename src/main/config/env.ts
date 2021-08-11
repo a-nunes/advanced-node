3143a7fbe28e3e6a73c7558f802d480c89718752
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-08-11 20:45:06 -0300

    test: ensure FacebookApi gets User Info on success

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID ?? '526246235153094',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? '399fe8814014aa97e936fa5a63121938',
+  },
+};
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,19 @@
-describe('Facebook Api Integration Tests', () => {
-  it('', async () => {
+import { FacebookApi } from '@/infra/apis';
+import { AxiosHttpClient } from '@/infra/http';
+import { env } from '@/main/config/env';
 
+describe('Facebook Api Integration Tests', () => {
+  it('should return User Info on success', async () => {
+    const axiosClient = new AxiosHttpClient();
+    const sut = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret);
+
+    // eslint-disable-next-line max-len
+    const fbUser = await sut.loadUser({ token: 'EAAHenjZCpIsYBAPQWC5EimlTi4wBjhbkpGcM2w84XL7YiIFcwTLO56085hfZCswn3AG1E3MsUUuLqrOiVDY2srMCwTPwup2uaCHkGAdPFNf4cTbw1gTHh01JBVp8Wwqzy4i4Ux8F4y1pWQ2I6ZC7b9rQBZCbFBQftf2vij2VYSXiZCQ9TALGrkAaNOHWSoYLUJMigBeXkkwZDZD' });
+
+    expect(fbUser).toEqual({
+      name: 'Artur Test',
+      email: 'artur_kkcqexm_test@tfbnw.net',
+      facebookId: '100653928990288',
+    });
   });
 });
