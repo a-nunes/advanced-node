077b4a202407dc1c944cfe2896ea01dee0c1734f
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-08-12 19:36:29 -0300

    build: add express, cors and helmet to the project

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,21 @@
+import './config/module-alias';
+
 import 'reflect-metadata';
+import express, { Router } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+const router = Router();
+
+router.post('/api/login/facebook', (_req, res) => {
+  res.send({ data: 'hello-world' });
+});
+
+app.use(router);
+
+app.listen(8080, () => console.log('Server running on http://localhost:8080'));
