4e1f2b924a4c483f236e2ae7ea0e1648894dfe9d
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-08-12 19:57:55 -0300

    refactor: change port to use env variable

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,4 +3,5 @@ export const env = {
     clientId: process.env.FB_CLIENT_ID ?? '526246235153094',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '399fe8814014aa97e936fa5a63121938',
   },
+  port: process.env.PORT ?? '8080',
 };
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 import './config/module-alias';
 import { app } from '@/main/config/app';
+import { env } from '@/main/config/env';
 
 import 'reflect-metadata';
 
-app.listen(8080, () => console.log('Server running on http://localhost:8080'));
+app.listen(env.port, () => console.log(`Server running on http://localhost:${env.port}`));
