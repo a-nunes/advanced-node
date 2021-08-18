cf42a3df2bdc9ed8552c6ca6babb1d0a9d46a408
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-08-18 20:35:45 -0300

    impr: make login route apply ExpressRouter adapter

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,7 +1,10 @@
+import { ExpressRouter } from '@/infra/http';
+import { makeFacebookLoginController } from '@/main/factories/controllers';
 import { Router } from 'express';
 
+const controller = makeFacebookLoginController();
+const fbLoginRouter = new ExpressRouter(controller);
+
 export default (router: Router): void => {
-  router.post('/login/facebook', (_req, res) => {
-    res.send({ data: 'hello-world' });
-  });
+  router.post('/login/facebook', fbLoginRouter.adapt);
 };
