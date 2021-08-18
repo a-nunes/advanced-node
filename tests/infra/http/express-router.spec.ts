c09a26b19476f1b83b1782c7ad06ee978041bdfe
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-08-18 20:21:05 -0300

    test: ensure ExpressRouter responds 200 and valid data

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -1,31 +1,55 @@
 import { Controller } from '@/application/controllers';
 
 import { Request, Response } from 'express';
-import { mock } from 'jest-mock-extended';
+import { mock, MockProxy } from 'jest-mock-extended';
 import { getMockReq, getMockRes } from '@jest-mock/express';
 
 class ExpressRouter {
   constructor(private readonly controller: Controller) {}
 
-  async handle(req: Request, _res: Response): Promise<void> {
-    this.controller.handle(req.body);
+  async adapt(req: Request, res: Response): Promise<void> {
+    const httpResponse = await this.controller.handle({ ...req.body });
+    res.status(httpResponse.statusCode).json(httpResponse.data);
   }
 }
 
 describe('ExpressRouter', () => {
-  it('should call handle with correct params', async () => {
-    const controller = mock<Controller>();
-    const req = getMockReq({
+  let controller: MockProxy<Controller>;
+  let req: Request;
+  let res: Response;
+  let sut: ExpressRouter;
+
+  beforeAll(() => {
+    controller = mock();
+    controller.handle.mockResolvedValue({
+      statusCode: 200,
+      data: { data: 'any_res_data' },
+    });
+    req = getMockReq({
       body: {
-        data: 'any_data',
+        data: 'any_req_data',
       },
     });
-    const { res } = getMockRes();
-    const sut = new ExpressRouter(controller);
+    res = getMockRes().res;
+  });
 
-    sut.handle(req, res);
+  beforeEach(() => {
+    sut = new ExpressRouter(controller);
+  });
 
-    expect(controller.handle).toHaveBeenCalledWith({ data: 'any_data' });
+  it('should call handle with correct params', async () => {
+    await sut.adapt(req, res);
+
+    expect(controller.handle).toHaveBeenCalledWith({ data: 'any_req_data' });
     expect(controller.handle).toHaveBeenCalledTimes(1);
+  });
+
+  it('should respond status 200 and valid data', async () => {
+    await sut.adapt(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ data: 'any_res_data' });
+    expect(res.json).toHaveBeenCalledTimes(1);
   });
 });
