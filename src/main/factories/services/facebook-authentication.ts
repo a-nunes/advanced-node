1c52246ea38ef910fc58ec876a0597f6674f76d4
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-08-18 19:29:17 -0300

    impr: create makeJwtTokenGenerator factory

--- src/main/factories/services/facebook-authentication.ts
+++ src/main/factories/services/facebook-authentication.ts
@@ -1,14 +1,14 @@
 import { FacebookAuthenticationService } from '@/data/services';
 import { FacebookApi } from '@/infra/apis';
-import { JwtTokenGenerator } from '@/infra/crypto';
 import { AxiosHttpClient } from '@/infra/http';
 import { PgUserAccountRepository } from '@/infra/postgres/repositories';
 import { env } from '@/main/config/env';
+import { makeJwtTokenGenerator } from '@/main/factories/crypto';
 
 export const makeFacebookAuthenticationService = (): FacebookAuthenticationService => {
   const axiosClient = new AxiosHttpClient();
   const fbApi = new FacebookApi(axiosClient, env.facebookApi.clientId, env.facebookApi.clientSecret);
   const pgUserAccountRepository = new PgUserAccountRepository();
-  const jwtTokenGenerator = new JwtTokenGenerator(env.jwtSecret);
+  const jwtTokenGenerator = makeJwtTokenGenerator();
   return new FacebookAuthenticationService(fbApi, pgUserAccountRepository, jwtTokenGenerator);
 };
