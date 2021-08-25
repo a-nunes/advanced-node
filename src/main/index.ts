004851d46c1c6061eaf2375d818ed966d0dc0266
Artur Ligieri Nunes <art.nunes@gmail.com>
2021-08-24 21:01:53 -0300

    impr: setup typeorm connection

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,13 @@
 /* eslint-disable no-console */
 import './config/module-alias';
 import 'dotenv/config';
+import { createConnection } from 'typeorm';
+
 import { app } from '@/main/config/app';
 import { env } from '@/main/config/env';
 
 import 'reflect-metadata';
 
-app.listen(env.port, () => console.log(`Server running on http://localhost:${env.port}`));
+createConnection()
+  .then(() => { app.listen(env.port, () => console.log(`Server running on http://localhost:${env.port}`)); })
+  .catch(console.error);
